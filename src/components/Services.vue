fffbe1b94e36196b9e020851ccecb57720e2d178
Frank Ilunga <ilungafrank3@gmail.com>
2022-11-22 17:09:56 +0300

    changes

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -7,7 +7,7 @@
     <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
       <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
           <div class="w-40 h-40 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
-          <img class="rounded-full w-40 h-40" src="src/assets/quality.png" alt="" srcset="">
+          <img class="rounded-full w-40 h-40" src="" alt="" srcset="">
         </div>
         <div class="flex-grow">
           <h2 class="text-[#A45C40] text-2xl title-font font-medium mb-3">Diving</h2>
@@ -21,7 +21,7 @@
       </div>
       <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
         <div class="w-40 h-40 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
-          <img class="rounded-full w-40 h-40" src="src/assets/quality.png" alt="" srcset="">
+          <img class="rounded-full w-40 h-40" src="" alt="" srcset="">
         </div>
         <div class="flex-grow">
           <h2 class="text-[#A45C40] text-2xl title-font font-medium mb-3">Snorkling</h2>
@@ -35,7 +35,7 @@
       </div>
       <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
          <div class="w-40 h-40 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
-          <img class="rounded-full w-40 h-40" src="src/assets/quality.png" alt="" srcset="">
+          <img class="rounded-full w-40 h-40" src="" alt="" srcset="">
         </div>
       
         <div class="flex-grow">
@@ -51,7 +51,7 @@
       <div class="md:flex justify-center">
           <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
           <div class="w-40 h-40 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
-          <img class="rounded-full w-40 h-40" src="src/assets/quality.png" alt="" srcset="">
+          <img class="rounded-full w-40 h-40" src="" alt="" srcset="">
         </div>
         <div class="flex-grow">
           <h2 class="text-[#A45C40] text-2xl title-font font-medium mb-3">Boat ride to Mahale National Park</h2>
@@ -65,7 +65,7 @@
       </div>
       <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
           <div class="w-40 h-40 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
-          <img class="rounded-full w-40 h-40" src="src/assets/quality.png" alt="" srcset="">
+          <img class="rounded-full w-40 h-40" src="" alt="" srcset="">
         </div>
         <div class="flex-grow">
           <h2 class="text-[#A45C40] text-2xl title-font font-medium mb-3">Boat ride to Gombe National Park</h2>
