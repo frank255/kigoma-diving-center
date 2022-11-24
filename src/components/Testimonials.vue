6c73c98c9dded50bdec2348012d3687a413c4450
Frank Ilunga <ilungafrank3@gmail.com>
2022-11-24 18:00:46 +0300

    chnages to team

--- src/components/Testimonials.vue
+++ src/components/Testimonials.vue
@@ -1,90 +1,25 @@
 <template>
   <section class="my-8">
-    <div
-      class="
-        container
-        mx-auto
-        flex flex-col
-        items-center
-        pb-6
-        mb-4
-        md:p-10 md:px-12
-      "
-    >
+    <div class=" container mx-auto  flex flex-col  items-center  pb-6 mb-4 md:p-10 md:px-12" >
       <h1
-        class="
-          text-3xl
-          sm:text-5xl
-          font-semibold
-          leading-none
-          text-[#A45C40] text-center
-        "
+        class=" text-3xl sm:text-5xl font-semibold leading-none text-[#A45C40] text-center"
       >
         What our customers are saying about us
       </h1>
     </div>
-    <div
-      class="
-        w-full
-        mx-auto
-        grid grid-cols-1
-        gap-8
-        lg:gap-20
-        md:px-10 md:pb-10
-        lg:grid-cols-2
-      "
-    >
-      <div
-        class="flex flex-col z-10 items-center mx-12 lg:mx-0"
-        v-for="comment in comments"
-        :key="comment.id"
-      
-      >
-        <div class="relative text-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            fill="currentColor"
-            class="absolute top-0 left-0 w-8 h-8 dark:text-gray-700"
-          >
-            <path
-              d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"
-            ></path>
-            <path
-              d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"
-            ></path>
-          </svg>
-          <p class="px-6 py-1 text-lg italic">
-            &nbsp;&nbsp;{{ comment.comment }}&nbsp;&nbsp;
-          </p>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            fill="currentColor"
-            class="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700"
-          >
-            <path
-              d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"
-            ></path>
-            <path
-              d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"
-            ></path>
-          </svg>
+    <figure class="mx-auto max-w-screen-md text-center mb-10"  v-for="comment in comments" :key="comment.id">
+    <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/></svg>
+    <blockquote>
+        <p class="text-2xl italic font-medium text-gray-900 dark:text-white">"{{ comment.comment }}"</p>
+    </blockquote>
+    <figcaption class="flex justify-center items-center mt-6 space-x-3">
+        <img class="w-10 h-10 rounded-full" src="src/assets/speech-bubble.gif" alt="profile picture">
+        <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+            <cite class="pr-3 font-medium text-gray-900 dark:text-white">{{comment.fullname}}</cite>
+            <cite class="pl-3 text-sm font-light text-gray-500 dark:text-gray-400">{{ comment.title}}</cite>
         </div>
-        <span
-          class="
-            w-12
-            h-1
-            my-2
-            rounded-lg
-            bg-gradient-to-r
-            from-violet-500
-            to-fuchsia-500
-          "
-        ></span>
-        <p>{{ comment.fullname }},{{ comment.title }}</p>
-      </div>
-    </div>
+    </figcaption>
+</figure>
   </section>
 </template>
 
