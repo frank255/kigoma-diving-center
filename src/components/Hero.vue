bf96eb1e08f52a884de3869ce61324b13285c147
Frank Ilunga <ilungafrank3@gmail.com>
2022-12-23 21:05:28 +0300

    addition of galery

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -35,65 +35,6 @@
     </div>
 </section>
 
-<!-- Container for demo purpose -->
-<div>
-
-<!-- Section: Design Block -->
-<section class="mb-40">
-
-
-  <svg
-    data-name="Layer 1"
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 1440 320"
-    preserveAspectRatio="none"
-    class="svg absolute hidden lg:block"
-    style="height: 560px; width: 100%; z-index: -10; overflow: hidden"
-  >
-    <defs>
-      <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
-        <stop stop-color="hsl(217, 102%, 99%)" offset="0%"></stop>
-        <stop stop-color="hsl(217,88%, 93%)" offset="100%"></stop>
-      </linearGradient>
-    </defs>
-    <path
-      fill="url(#sw-gradient-0)"
-      d="M 0.351 264.418 C 0.351 264.418 33.396 268.165 47.112 270.128 C 265.033 301.319 477.487 325.608 614.827 237.124 C 713.575 173.504 692.613 144.116 805.776 87.876 C 942.649 19.853 1317.845 20.149 1440.003 23.965 C 1466.069 24.779 1440.135 24.024 1440.135 24.024 L 1440 0 L 1360 0 C 1280 0 1120 0 960 0 C 800 0 640 0 480 0 C 320 0 160 0 80 0 L 0 0 L 0.351 264.418 Z"
-    ></path>
-  </svg>
-
-  <div class="px-6 py-12 lg:my-12 md:px-12 text-gray-800 text-center lg:text-left">
-    <div class="container mx-auto xl:px-32">
-      <div class="grid lg:grid-cols-2 gap-12 flex items-center">
-        <div class="mt-12 lg:mt-0">
-          <h1 class="text-4xl md:text-5xl xl:text-6xl font-bold tracking-tight mb-12">Are you ready <br /><span class="text-blue-600">for an adventure?</span></h1>
-          <div class="md:flex flex-row">
-            <input type="text" class="form-control block w-full px-4 py-2 mb-2 md:mb-0 md:mr-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Enter your email" />
-            <button type="submit" class="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">Subscribe</button>
-          </div>
-        </div>
-        <div class="mb-12 lg:mb-0">
-          <div
-            class="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden rounded-lg shadow-lg"
-            style="padding-top: 56.25%"
-          >
-            <iframe
-              class="embed-responsive-item absolute top-0 right-0 bottom-0 left-0 w-full h-full"
-              src="https://www.youtube.com/embed/vlDzYIIOYmM?enablejsapi=1&amp;origin=https%3A%2F%2Fmdbootstrap.com"
-              allowfullscreen=""
-              data-gtm-yt-inspected-2340190_699="true"
-              id="240632615"
-            ></iframe>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-<!-- Section: Design Block -->
-
-</div>
-<!-- Container for demo purpose -->
 </template>
 
 <script setup>
