87af4b6159ca26aa0992ebaf44d9d4fc88667238
Frank Ilunga <ilungafrank3@gmail.com>
2024-07-01 07:42:02 +0300

    changes to hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,3 @@
-<script setup>
-import Services from "../components/Services.vue";
-import Aboutus from "../components/Aboutus.vue";
-import Contacts from "../components/Contacts.vue";
-import Faqs from "../components/Faqs.vue";
-import Team from "../components/Team.vue";
-import Testimonials from "../components/Testimonials.vue";
-import Hero from "../components/Hero.vue";
-</script>
 <template>
   <div class=" sm:max-w-xl md:max-w-full lg:max-w-screen-xl">
     <Hero />
@@ -18,3 +9,12 @@ import Hero from "../components/Hero.vue";
     <Faqs />
   </div>
 </template>
+<script setup>
+import Services from "../components/Services.vue";
+import Aboutus from "../components/Aboutus.vue";
+import Contacts from "../components/Contacts.vue";
+import Faqs from "../components/Faqs.vue";
+import Team from "../components/Team.vue";
+import Testimonials from "../components/Testimonials.vue";
+import Hero from "../components/Hero.vue";
+</script>
